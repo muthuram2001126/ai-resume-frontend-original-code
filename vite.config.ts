b3ea9762a001977a89f8c7a4ed49dc791c3d6c90
MuthuramMuruganandan <muthurammuruganandan@gmail.com>
2025-09-21 14:11:15 +0530

    5th commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ai-resume-frontend-original-code/',   // <--- important
   server: {
     host: "::",
     port: 8080,
